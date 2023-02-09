414d0f48fa23c45dff67e721e8ee53546f680e3a
[priyankasingh] <priyanka-pixel>
2023-02-09 19:11:11 +0000

    Database

--- app/src/main/java/com/example/disneyapp/data/remote/dto/DisneyDto.kt
+++
@@ -1,3 +0,0 @@
-package com.example.disneyapp.data.remote.dto
-
-class DisneyDto : ArrayList<DisneyDtoItem>()
--- app/src/main/java/com/example/disneyapp/domain/model/DisneyList.kt
+++ app/src/main/java/com/example/disneyapp/domain/model/DisneyList.kt
@@ -1,3 +1,11 @@
 package com.example.disneyapp.domain.model
 
-data class DisneyList()
+data class DisneyList(
+    val id: Int,
+    val name: String,
+    val playtime: String,
+    val plot: String,
+    val poster: String,
+    val release: String,
+    var isFavorite:Boolean = false
+)
